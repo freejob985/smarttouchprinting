a6f10328e91703006a914c7044ce388a729154c2
freejob985 <freejob985@gmail.com>
2021-01-14 02:51:32 +0200

    asd

--- resources/views/web/detail.blade.php
+++ resources/views/web/detail.blade.php
@@ -1,6 +1,9 @@
 @extends('web.layout')
 @section('content')
-@php $r =   'web.details.detail' . $final_theme['detail']; @endphp
+@php 
+dd($final_theme['detail']);
+$r =   'web.details.detail' . $final_theme['detail'];
+ @endphp
 @include($r)
 @include('web.common.scripts.addToCompare')
 @endsection
